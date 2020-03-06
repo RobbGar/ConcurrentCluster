6a80f472d76e41d166dc58b71c214458b6652ead
RobbGar <49005244+RobbGar@users.noreply.github.com>
2020-03-06 12:01:18 +0100

    New GUI for server, including new icons, a new menu bar & dark mode!

---
+++ server/ServerController.java
@@ -0,0 +1,98 @@
+package server;
+
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class ServerController {
+    private ServerGUI view;
+    private Server server;
+
+
+    private ServerController(ServerGUI v){
+        view = v;
+        initController();
+    }
+
+    private void initController(){
+        server  = new Server(view);
+
+        view.getDarkMode().addItemListener((e) -> {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                view.getContentPane().setBackground(Color.decode("#37474f"));
+                view.getTextArea().setBackground(Color.decode("#62727b"));
+                view.getTextArea().setForeground(Color.WHITE);
+                view.getLblAddr().setForeground(Color.WHITE);
+                view.getLblIndirizzoIpServer().setForeground(Color.WHITE);
+
+
+            }
+        });
+
+        view.getLightMode().addItemListener((e) -> {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                view.getContentPane().setBackground(Color.decode("#f5f5f5"));
+                view.getTextArea().setBackground(Color.WHITE);
+                view.getTextArea().setForeground(Color.BLACK);
+                view.getLblAddr().setForeground(Color.BLACK);
+                view.getLblIndirizzoIpServer().setForeground(Color.BLACK);
+
+            }
+        });
+
+        view.getExit().addActionListener(e -> System.exit(0));
+
+        view.getInfoItem().addActionListener(e ->
+                JOptionPane.showMessageDialog(view,
+                                      "Created by Roberto Garbarino, NotMyUid & Cristiano Olivari",
+                                         "Info",
+                                             JOptionPane.PLAIN_MESSAGE,
+                                             new ImageIcon("server/resources/ico.png")
+                                                ));
+
+        view.getReset().addActionListener(e -> {
+                    if (!server.resetData())
+                        view.update("Error: could not reset data");
+                    else
+                        view.update("Reset data");
+                }
+        );
+        view.getLoad().addActionListener(e -> {
+                    if (server.loadData())
+                        view.update("Data loaded");
+                    else
+                        view.update("Error loading Data");
+            }
+
+        );
+        view.getSave().addActionListener(e -> {
+            if (server.saveData())
+                view.update("Data saved");
+            else
+                view.update("Error saving data");
+        });
+
+        view.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+    }
+
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                ServerGUI frame = new ServerGUI();
+                ServerController c = new ServerController(frame);
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
--- server/ServerGUI.java
+++ server/ServerGUI.java
@@ -3,6 +3,8 @@ package server;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import java.awt.event.KeyEvent;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -11,61 +13,156 @@ public class ServerGUI extends JFrame {
 	protected Server server;
 	private JPanel contentPane;
 	private JTextArea textArea;
-    private JLabel lblIndirizzoIpServer;
-    private JLabel lblAddr;
-	private JButton rstBtn;
-	private JButton loadBtn;
-	private JButton saveBtn;
+	private JLabel lblIndirizzoIpServer;
+	private JLabel lblAddr;
 
-	JButton getLoadBtn() {
-		return loadBtn;
+
+	public JMenuBar getMenuBarr() {
+		return menuBar;
 	}
 
-	JButton getSaveBtn() {
-		return saveBtn;
+	private JMenuBar menuBar;
+	private JMenu file;
+	private JMenu settings;
+	private JMenu view;
+	private JMenu info;
+	private JMenuItem infoItem;
+	private JMenuItem exit;
+	private JMenuItem save;
+	private JMenuItem load;
+	private JMenuItem reset;
+	private JMenuItem settingsItem;
+	private ButtonGroup bg;
+	private JRadioButtonMenuItem lightMode;
+	private JRadioButtonMenuItem darkMode;
+
+	/*Getters*/
+	public JTextArea getTextArea() {
+		return textArea;
 	}
 
-	JButton getRstBtn() {
-		return rstBtn;
+	public JLabel getLblIndirizzoIpServer() {
+		return lblIndirizzoIpServer;
 	}
+
+	public JLabel getLblAddr() {
+		return lblAddr;
+	}
+
+	public JMenuItem getInfoItem() {
+		return infoItem;
+	}
+
+	public JMenuItem getExit() {
+		return exit;
+	}
+
+	public JMenuItem getSave() {
+		return save;
+	}
+
+	public JMenuItem getLoad() {
+		return load;
+	}
+
+	public JMenuItem getReset() {
+		return reset;
+	}
+
+	public JRadioButtonMenuItem getLightMode() {
+		return lightMode;
+	}
+
+	public JRadioButtonMenuItem getDarkMode() {
+		return darkMode;
+	}
+
 
 	public ServerGUI() throws UnknownHostException {
 		setBounds(100, 100, 512, 497);
+		getContentPane().setBackground(Color.WHITE);
 
-		ImageIcon img = new ImageIcon("ico.png");
-		setIconImage(img.getImage());
+		/*Beginning Menu Bar*/
+		menuBar = new JMenuBar();
+			//File menu with save load and reset options
+		file = new JMenu("File");
+		save = new JMenuItem("Save data", new ImageIcon("server/resources/save.png"));
+		load = new JMenuItem("Load data", new ImageIcon("server/resources/load.png"));
+		reset = new JMenuItem("Reset data", new ImageIcon("server/resources/reset.png"));
+
+		exit = new JMenuItem("Exit", new ImageIcon("server/resources/exit.png"));
+		file.setMnemonic(KeyEvent.VK_F);
+		exit.setMnemonic(KeyEvent.VK_E);
+		exit.setToolTipText("Exit application");
+
+		file.add(save);
+		file.add(load);
+		file.add(reset);
+		file.addSeparator();
+		file.add(exit);
+
+		menuBar.add(file);
+
+		//Settings Menu
+		settings = new JMenu("Settings");
+		settingsItem = new JMenuItem("Preferences", new ImageIcon("server/resources/settings.png"));
+
+		settings.setMnemonic(KeyEvent.VK_F);
+		settingsItem.setMnemonic(KeyEvent.VK_E);
+
+		settings.add(settingsItem);
+		menuBar.add(settings);
+
+		//Light and Dark mode settings
+		view = new JMenu("View");
+		bg = new ButtonGroup();
+		lightMode  = new JRadioButtonMenuItem("Light Mode");
+		lightMode.setSelected(true);
+		bg.add(lightMode);
+
+		darkMode  = new JRadioButtonMenuItem("Dark Mode");
+		bg.add(darkMode);
+		view.add(lightMode);
+		view.add(darkMode);
+
+		menuBar.add(view);
+
+		//Info Menu
+		info = new JMenu("Info");
+		info.setMnemonic(KeyEvent.VK_F);
+		infoItem = new JMenuItem("Info");
+		infoItem.setMnemonic(KeyEvent.VK_E);
+		info.add(infoItem);
+		menuBar.add(Box.createHorizontalGlue());
+		menuBar.add(info);
+
+		setJMenuBar(menuBar);
+
+		/*End of Menu bar*/
+
+		ImageIcon icon = new ImageIcon("ico.png");
+		setIconImage(icon.getImage());
 		setResizable(false);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		contentPane.setBackground(Color.decode("#f5f5f5"));
 
 		lblAddr = new JLabel("IP Address:");
 		lblAddr.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
-		lblAddr.setBounds(22, 6, 172, 14);
+		lblAddr.setBounds(22, 13, 172, 14);
 		contentPane.add(lblAddr);
 
 		InetAddress inetAddress = InetAddress.getLocalHost();
         String iA=inetAddress.getHostAddress();
 		lblIndirizzoIpServer = new JLabel(iA);
 		lblIndirizzoIpServer.setFont(new Font("Lucida Grande", Font.BOLD, 15));
-		lblIndirizzoIpServer.setBounds(42, 26, 172, 14);
+		lblIndirizzoIpServer.setBounds(110, 14, 172, 14);
 		contentPane.add(lblIndirizzoIpServer);
 
-		rstBtn = new JButton("RESET");
-		rstBtn.setBounds(360, 16, 80, 30);
-		contentPane.add(rstBtn);
-
-		loadBtn = new JButton("LOAD");
-		loadBtn.setBounds(260, 16, 80, 30);
-		contentPane.add(loadBtn);
-
-		saveBtn = new JButton("SAVE");
-		saveBtn.setBounds(160, 16, 80, 30);
-		contentPane.add(saveBtn);
-		
 		textArea = new JTextArea(160, 250);
-		textArea.setBounds(22, 60, 458, 390);
+		textArea.setBounds(22, 40, 458, 390);
 		contentPane.add(textArea);
 		textArea.setEditable(false);
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
